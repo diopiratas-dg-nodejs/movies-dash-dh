84c6631b3c975fbc767381a9d3bfcd0f4c423180
Diogenes de Carvalho Pereira <diogenes.pereira@luizalabs.com>
2022-12-15 19:45:01 -0300

    init: initial commit

---
+++ src/components/ContentRowTop.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ContentRowMovies from './ContentRowMovies';
+import LastMovieInDb from './LastMovieInDb';
+import GenresInDb from './GenresInDb';
+
+function ContentRowTop(){
+    return (
+        <React.Fragment>
+            {/*<!-- Content Row Top --> */}
+				<div className="container-fluid">
+					<div className="d-sm-flex align-items-center justify-content-between mb-4">
+						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+					</div>
+				
+					<ContentRowMovies />					
+	
+					{/*<!-- Content Row Last Movie in Data Base --> */}
+					<div className="row">
+						<LastMovieInDb />
+
+						{/*<!-- Genres in DB --> */}
+						<GenresInDb />
+					</div>
+				</div>
+				{/*<!--End Content Row Top--> */}
+        </React.Fragment>
+    )
+}
+
+export default ContentRowTop;
---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TopBar from './TopBar';
+import Footer from './Footer';
+import ContentRowTop from './ContentRowTop';
+
+function ContentWrapper(){
+    return (
+        <React.Fragment>
+            {/*<!-- Content Wrapper --> */}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Main Content --> */}
+                <div id="content">
+                    <TopBar />
+                    <ContentRowTop />
+                    <Footer />
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default ContentWrapper;
